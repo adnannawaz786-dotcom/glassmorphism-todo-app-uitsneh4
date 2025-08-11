aac32b1864f49c0ce247bf959af4cebddaf33fee
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-11 21:13:08 +0100

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+```javascript
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/components': path.resolve(__dirname, './components'),
+      '@/lib': path.resolve(__dirname, './lib'),
+    },
+  },
+  css: {
+    postcss: './postcss.config.js',
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+})
+```
